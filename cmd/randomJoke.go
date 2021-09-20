3121bdaf7e9d07570c25f12d3aee966bfe6da794
Dmytro Halkin <halkin.dmytro@pdffiller.team>
2021-09-21 00:46:01 +0300

    setup cobra config file

--- cmd/randomJoke.go
+++ cmd/randomJoke.go
@@ -1,24 +1,10 @@
-/*
-Copyright © 2021 NAME HERE <EMAIL ADDRESS>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package cmd
 
 import (
 	"encoding/json"
 	"fmt"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 	"io/ioutil"
 	"net/http"
 )
@@ -49,8 +35,12 @@ func init() {
 }
 
 func getRandomJoke() {
-	url := "https://icanhazdadjoke.com/"
-	responseBytes := getJokeData(url)
+
+	url1 := viper.Get("api.dadjoke.baseurl")
+	// convert to string ?
+	url2 := fmt.Sprintf("%v", url1)
+
+	responseBytes := getJokeData(url2)
 	joke := Joke{}
 	if err := json.Unmarshal(responseBytes, &joke); err != nil {
 		fmt.Println("Could not unmarshall response - %v", err)
--- cmd/root.go
+++ cmd/root.go
@@ -1,28 +1,12 @@
-/*
-Copyright © 2021 NAME HERE <EMAIL ADDRESS>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package cmd
 
 import (
 	"fmt"
+	"github.com/dhalkin/go-game/internal/projectpath"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"os"
 )
-
-var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -48,8 +32,6 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.go-game.yaml)")
-
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
@@ -57,21 +39,16 @@ func init() {
 
 // initConfig reads in config file and ENV variables if set.
 func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := os.UserHomeDir()
-		cobra.CheckErr(err)
-
-		// Search config in home directory with name ".go-game" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigType("yaml")
-		viper.SetConfigName(".go-game")
-	}
+	// Search config in home directory with name ".go-game" (without extension).
+	viper.AddConfigPath(projectpath.Root)
+	viper.SetConfigType("yaml")
+	viper.SetConfigName(".go-game")
 
 	viper.AutomaticEnv() // read in environment variables that match
+
+	//there are no keys, why?
+	//fmt.Println("viper allKeys:", viper.AllKeys())
+	//fmt.Println("project path:", projectpath.Root)
 
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
